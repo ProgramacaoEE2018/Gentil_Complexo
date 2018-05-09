ad3289b1eb9612168e71ab4cdb63dff826cd1f99
GentilGustavo <gustavogentil44@gmail.com>
2018-05-09 15:05:08 -0300

    Forth Commit

--- TESTE_COMPLEXOS.cpp
+++ TESTE_COMPLEXOS.cpp
@@ -6,86 +6,65 @@ int main() {
 	Complexo z1(3, 4);
 	Complexo z2(3, 4);
 	Complexo conjugado(3, -4);
-	Complexo diverenca(0, 0);
 	Complexo divisao(1, 0);
 	Complexo produto(-7, 24);
 	Complexo soma(6, 8);
-	Complexo Teste(0, 0);
-	int erro;
 	float modulo;
-
-	erro = 0;
-	
-
-	//Teste Construtor
-	if ( z2 == z1) {
-		printf("Construtor OK\n");
-	}
-	else {
-		printf("Falha Construtor\n");
-		erro = erro + 1;
-	}
-
-	//Teste Conjugado
-	Teste = z1.Conjugado();
-	if (Teste == conjugado) {
-		printf("Conjugado OK\n");
-	}
-	else {
-		printf("Falha Conjugado\n");
-		erro = erro + 2;
-	}
-
-	//Teste Diferenca
-	Teste = z1 - z2;
-	if (Teste == diverenca) {
-		printf("Diferenca OK\n");
-	}
-	else {
-		printf("Falha Difernca\n");
-		erro = erro + 4;
-	}
-	
-	//Teste Divisao
-	Teste = z1 / z2;
-	if (Teste == divisao) {
-		printf("Divisao OK\n");
-	}
-	else {
-		printf("Falha Divisao\n");
-		erro = erro + 8;
-	}
 
 	//Teste Modulo
 	modulo = z1.Modulo();
-	if ( modulo == 5) {
+	if (modulo == 5) {
 		printf("Modulo OK\n");
+		
+		//Teste Diferenca
+		modulo = (z1 - z2).Modulo()
+			if (modulo == 0) {
+				printf("Diferenca OK\n");
+			}
+			else {
+				printf("Falha Difernca\n");
+			}
+		
+		//Teste Divisao
+		modulo = ((z1 / z2) - divisao).Modulo;
+		if (modulo == 0) {
+			printf("Divisao OK\n");
+		}
+		else {
+			printf("Falha Divisao\n");
+		}
+
+		//Teste Conjugado
+		modulo = (z1.Conjugado - conjugado).Modulo;
+		if (modulo == 0) {
+			printf("Conjugado OK\n");
+		}
+		else {
+			printf("Falha Conjugado\n");
+		}
+
+		//Teste Produto
+		modulo = ((z1 * z2) - produto).Modulo;
+		if (modulo == 0) {
+			printf("produto OK\n");
+		}
+		else {
+			printf("Falha produto\n");
+		}
+
+		//Teste Soma
+		modulo = ((z1 + z2) - soma).Modulo;
+		if (modulo == 0) {
+			printf("Soma OK\n");
+		}
+		else {
+			printf("Falha Soma\n");
+		}
 	}
 	else {
 		printf("Falha Modulo\n");
-		erro = erro + 16;
 	}
 
-	//Teste Produto 
-	Teste = z1 * z2;
-	if (Teste == produto) {
-		printf("Produto OK\n");
-	}
-	else {
-		printf("Falha Produto\n");
-		erro = erro + 32;
-	}
-
-	//Teste Soma
-	Teste = z1 + z2;
-	if (Teste == soma) {
-		printf("Soma OK\n");
-	}
-	else {
-		printf("Falha Soma\n");
-		erro = erro + 64;
-	}
-
-
-	return erro;
+	
+	return 0;
 }
