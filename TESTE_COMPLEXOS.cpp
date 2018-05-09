6ca62a1b2f2cc45dd25aa4c3205be472db55c8ad
GentilGustavo <gustavogentil44@gmail.com>
2018-05-09 14:35:46 -0300

    Thirt Commit

--- TESTE_COMPLEXOS.cpp
+++ TESTE_COMPLEXOS.cpp
@@ -3,83 +3,86 @@
 
 
 int main() {
-	Complexo z1, z2, Teste;
+	Complexo z1(3, 4);
+	Complexo z2(3, 4);
+	Complexo conjugado(3, -4);
+	Complexo diverenca(0, 0);
+	Complexo divisao(1, 0);
+	Complexo produto(-7, 24);
+	Complexo soma(6, 8);
+	Complexo Teste(0, 0);
 	int erro;
 	float modulo;
 
 	erro = 0;
-	z1.real = 3;
-	z1.im = 4;
-	z2.real = 6;
-	z2.im = 8;
+	
 
 	//Teste Construtor
-	Teste.Complexo(42, 24);
-	if ( Teste.real == 42 && Teste.im == 24) {
-		printf("Construtor OK");
+	if ( z2 == z1) {
+		printf("Construtor OK\n");
 	}
 	else {
-		printf("Falha Construtor");
+		printf("Falha Construtor\n");
 		erro = erro + 1;
 	}
 
 	//Teste Conjugado
 	Teste = z1.Conjugado();
-	if (Teste.real == 3 && Teste.im == -4) {
-		printf("Conjugado OK");
+	if (Teste == conjugado) {
+		printf("Conjugado OK\n");
 	}
 	else {
-		printf("Falha Conjugado");
+		printf("Falha Conjugado\n");
 		erro = erro + 2;
 	}
 
 	//Teste Diferenca
 	Teste = z1 - z2;
-	if (Teste.real == -3 && Teste.im == -4) {
-		printf("Diferenca OK");
+	if (Teste == diverenca) {
+		printf("Diferenca OK\n");
 	}
 	else {
-		printf("Falha Difernca");
+		printf("Falha Difernca\n");
 		erro = erro + 4;
 	}
 	
 	//Teste Divisao
 	Teste = z1 / z2;
-	if (Teste.real == 0.5 && Teste.im == 0) {
-		printf("Divisao OK");
+	if (Teste == divisao) {
+		printf("Divisao OK\n");
 	}
 	else {
-		printf("Falha Divisao");
+		printf("Falha Divisao\n");
 		erro = erro + 8;
 	}
 
 	//Teste Modulo
 	modulo = z1.Modulo();
 	if ( modulo == 5) {
-		printf("Modulo OK");
+		printf("Modulo OK\n");
 	}
 	else {
-		printf("Falha Modulo");
+		printf("Falha Modulo\n");
 		erro = erro + 16;
 	}
 
 	//Teste Produto 
 	Teste = z1 * z2;
-	if (Teste.real == -14 && Teste.im == 24) {
-		printf("Produto OK");
+	if (Teste == produto) {
+		printf("Produto OK\n");
 	}
 	else {
-		printf("Falha Produto");
+		printf("Falha Produto\n");
 		erro = erro + 32;
 	}
 
 	//Teste Soma
 	Teste = z1 + z2;
-	if (Teste.real == 9 && Teste.im == 12) {
-		printf("Soma OK");
+	if (Teste == soma) {
+		printf("Soma OK\n");
 	}
 	else {
-		printf("Falha Soma");
+		printf("Falha Soma\n");
 		erro = erro + 64;
 	}
 
